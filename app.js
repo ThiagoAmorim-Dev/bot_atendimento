54782e01d1d750dce5ddca0339b3526b8448e165
Thiago Amorim <tamorimdealmeida@gmail.com>
2024-06-29 15:40:31 -0300

    feat: first version complete

--- app.js
+++ app.js
@@ -1,32 +1,26 @@
 const venom = require('venom-bot');
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
-const {validarData, diaDaSemana, disponivel} = require('./dataService');
+const { validarData, diaDaSemana, disponivel, obterHorarioDisponivel } = require('./dataService');
 const { formatarNumerosMensagem } = require('./util');
-
 
 venom.create({
     session: "Barbearia-BOT",
-    multidevice: true
+    multidevice: true,
 })
 .then((client) => start(client))
 .catch((err) => console.log(err));
 
-
 // URL da API SheetsDB
-const sheetsDbUrl = 'https://sheetdb.io/api/v1/sapz5eicgzz66';
-
-// horários disponíveis 
-var horarios = ["9:00h", "10:00h", "11:00h", "13:00h", "14:00h", "15:00h", "16:00h", "17:00h", "18:00h", "19:00h"];
-horarios = formatarNumerosMensagem(horarios);
+const sheetsDbUrl = 'https://api.sheety.co/be2a6b443056f481f94cc19d22b20b70/barbearia/';
+var horarios = [];
 
 // estados de conversação
 var state = {};
 
 // função de início do atendimento
 const start = (client) => {
-
-    client.onMessage((message) => {
+    client.onMessage(async (message) => {
         const chatId = message.from;
 
         // inicializa o estado do chat se não existir
@@ -36,9 +30,13 @@ const start = (client) => {
 
         const currentState = state[chatId];
 
+        if (message.body === '0'){
+            currentState.step = 0;
+        }
+
         // início do atendimento
         if (currentState.step === 0) {
-            client.sendText(chatId, "*Olá! Bem-vindo ao atendimento automático da barbearia West.* \n\n*O que deseja?* \n\n1️⃣ *Marcar um horário*\n2️⃣ *Ver serviços e preços*\n3️⃣ *Desmarcar Agendamento*");
+            client.sendText(chatId, "*Olá! Bem-vindo ao atendimento automático da barbearia West.* \n\n*O que deseja?* \n\n1️⃣ *Marcar um horário*\n2️⃣ *Ver serviços e preços*\n3️⃣ *Desmarcar Agendamento*\n\n *Digite 0 caso queira voltar para o menu a qualquer momento*");
 
             currentState.telefone = message.from;
 
@@ -56,41 +54,42 @@ const start = (client) => {
             } else if (message.body === '2') {
                 currentState.servicoSelecionado = "ver serviços e preços";
 
+                let response = await axios.get(`${sheetsDbUrl}servicos`);
+                let servicos = response.data.servicos;
+                let mensagemServicos = "";
+
+                servicos.forEach(servico => {
+                    mensagemServicos += `\n*Serviço: ${servico.servico}*\n*Valor: R$${servico.valor} reais*\n`
+                });
+
                 // Mensagem de gancho para a etapa 2 
-                client.sendText(chatId, "*Aqui estão nossos serviços e preços:* \n\n*Corte de cabelo: R$30* \n*Barba: R$20*\n*Combo (Corte + Barba): R$45* \n\n*Deseja Agendar?* \n1️⃣Sim \n2️⃣Não ");
-                currentState.step = 2; 
+                client.sendText(chatId, "*Aqui estão nossos serviços e preços:*\n" + mensagemServicos + "\n\n*Deseja Agendar?* \n*1️⃣Sim* \n*2️⃣Não*");
+                currentState.step = 2;
 
             } else if (message.body === '3') {
                 // verifica se na planilha existe algum horário marcado associado ao número 
                 currentState.servicoSelecionado = "desmarcar horario";
                 currentState.step = 2;
 
-                axios.get(`${sheetsDbUrl}/search?Telefone=${currentState.telefone}`)
-                .then(response => {
-                    if (response.data.length > 0) {
-                        currentState.agendamentos = response.data;
+                let response = await axios.get(`${sheetsDbUrl}/agendamento`);
 
-                        // mensagem gancho para a etapa 3
-                        let agendamentosMensagem = "*Encontrei os seguintes horários marcados*";
+                if (response.data.agendamento.length > 0) {
+                    currentState.agendamentos = response.data.agendamento;
 
-                        currentState.agendamentos.forEach((agendamento, index) => {
-                            agendamentosMensagem += `\n*${index + 1}. ${agendamento.Data} às ${agendamento.Horario} para ${agendamento.Nome}*`
-                        });
-                        agendamentosMensagem += "\n*Informe o número do agendamento que deseja desmarcar*";
+                    // mensagem gancho para a etapa 3
+                    let agendamentosMensagem = "*Encontrei os seguintes horários marcados*";
 
-                        client.sendText(chatId, agendamentosMensagem);
-                        currentState.step = 3;
-                    } else {
-                        client.sendText(chatId, "Você não possui nenhum horário marcado.");
-                        currentState.step = 0;
-                    }
-                })
-                .catch(error => {
-                    client.sendText(chatId, "*Houve um erro ao consultar sua agenda. Por favor, tente novamente mais tarde.*");
+                    currentState.agendamentos.forEach((agendamento, index) => {
+                        agendamentosMensagem += `\n\n*${index + 1}. ${agendamento.data} às ${agendamento.horario} para ${agendamento.nome}*`
+                    });
+                    agendamentosMensagem += "\n*Informe o número do agendamento que deseja desmarcar*";
 
-                    // volta para o menu principal
+                    client.sendText(chatId, agendamentosMensagem);
+                    currentState.step = 3;
+                } else {
+                    client.sendText(chatId, "Você não possui nenhum horário marcado.");
                     currentState.step = 0;
-                })
+                }
 
             } else {
                 client.sendText(chatId, "Perdão, não entendi a mensagem.");
@@ -101,80 +100,92 @@ const start = (client) => {
         } else if (currentState.step === 2) {
             // MARCAR HORÁRIO
             if (currentState.servicoSelecionado === "marcar horario") {
-
                 if (!validarData(message.body)) {
-                    client.sendText(chatId, `*Essa data não é válida. Por favor, digite novamente.*`);
-                }
-
-                else if (diaDaSemana(message.body) === "Domingo") {
-                    client.sendText(chatId, "*Infelizmente, não abrimos dia de domingo. Poderia escolher outra data? 😅*");
-
+                    client.sendText(chatId, `Essa data não é válida. Por favor, digite novamente.`);
+                } else if (diaDaSemana(message.body) === "Domingo") {
+                    client.sendText(chatId, "Infelizmente, não abrimos dia de domingo. Poderia escolher outra data? 😅");
                 } else {
-                    disponivel(message.body)
-                    .then(disponibilidade => {
-                        if (disponibilidade){
-                            currentState.data = message.body.toString();
-
-                            // formando os horários do dia especificado 
+                    let disponibilidade = await disponivel(message.body);
+                    if (disponibilidade){
+                        currentState.data = message.body.toString();
+                        horarios = await obterHorarioDisponivel(currentState.data);
+                        horarios = horarios.filter(horario => horario !== '');
+                        if (horarios.length > 0){
                             var horariosMensagem = "";
-                            horarios.forEach(horario => {
+                            let horariosformatados = formatarNumerosMensagem(horarios);
+                            horariosformatados.forEach(horario => {
                                 horariosMensagem += `*${horario}*\n`;
                             });
-        
                             // mensagem gancho para a etapa 3
                             client.sendText(chatId, "*Me informa o horário que deseja ⏰*\n\n"+horariosMensagem);
                             currentState.step = 3;
+                        } else {
+                            client.sendText(chatId, `*Infelizmente todos os horários para o dia ${currentState.data} já foram ocupados. Mas você pode digitar outra data ou digitar 0 para voltar para o menu.*`)
                         }
-                        else {
-                            client.sendText(chatId, `Na data ${message.body}, a barbearia vai estar fechada o dia todo. Poderia escolher outro data, por favor?`);
-                        }
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    })
+                    } else {
+                        client.sendText(chatId, `*Na data ${message.body}, a barbearia vai estar fechada o dia todo. Poderia escolher outro data, por favor?*`);
+                    }
                 }
-
             // VER SERVIÇOS E PREÇOS
             } else if (currentState.servicoSelecionado === "ver serviços e preços") {
                 if (message.body === "1") {
                     // vai para a primeira etapa do serviço "marcar horário"
-                    currentState.servicoSelecionado = "marcar horario"
+                    currentState.servicoSelecionado = "marcar horario";
                     client.sendText(chatId, "*Por favor, informe a data (dd/mm/aaaa) que deseja 🗓️*");
-                    currentState.step = 2; 
-                } else {
+                    currentState.step = 2;
+                } else if (message.body === '2'){
                     // volta para o menu principal
                     client.sendText(chatId, "*Sem problemas! Caso queira iniciar o atendimento novamente, basta enviar outra mensagem.*");
                     currentState.step = 0;
+                } else {
+                    client.sendText(chatId, "*Resposta inválida. Por favor, digite apenas 1 para marcar ou 2 para finalizar*");
+                    currentState.step = 2;
                 }
             }
 
+        //PASSO 3 
         } else if (currentState.step === 3) {
+            //MARCANDO HORÁRIO
             if (currentState.servicoSelecionado === "marcar horario") {
                 if (horarios[message.body - 1] != null) {
                     currentState.horarioEscolhido = horarios[parseInt(message.body) - 1];
-                    client.sendText(chatId, "*Agora me informe seu nome e sobrenome, por favor.*");
                     currentState.step = 4; 
-                    
+
+                    let response = await axios.get(`${sheetsDbUrl}servicos`);
+                    let contador = 0;
+                    var servicos = response.data.servicos;
+
+                    let mensagem_servicos = "";
+                    servicos.forEach(servico => {
+                        mensagem_servicos += ` *${contador + 1}. Serviço: ${servico.servico} | Preço: R$${servico.valor}*\n`
+                        contador++;
+                    });
+
+                    let mensagem_final = mensagem_servicos + '\n *Selecione o serviço que deseja. Você pode selecionar mais de um da seguinte forma: 1,2,3*'
+                    client.sendText(chatId, mensagem_final);
+
                 } else {
                     var horariosMensagem = "";
                     var ordem = 1;
+
+                    horarios = await obterHorarioDisponivel(currentState.data);
+                    
                     horarios.forEach(horario => {
                         horariosMensagem += `*${ordem}. ${horario}* *horas*\n`;
                         ordem++;
                     });
 
-                    client.sendText(chatId, "*Escolha um horário válido.*");
-                    client.sendText(chatId, "*Me informa o horário que deseja ⏰*\n\n"+horariosMensagem);
+                    client.sendText(chatId, "Escolha um horário válido.");
+                    client.sendText(chatId, "Me informa o horário que deseja ⏰\n\n"+horariosMensagem);
                 }
 
             } else if (currentState.servicoSelecionado === "desmarcar horario") {
                 const agendamentoEscolhidoIndex = parseInt(message.body) - 1;
-
                 if (currentState.agendamentos[agendamentoEscolhidoIndex]) {
                     let agendamentoEscolhido = currentState.agendamentos[agendamentoEscolhidoIndex];
-                    axios.delete(`${sheetsDbUrl}/Id/${agendamentoEscolhido.Id}`)
+                    axios.delete(`${sheetsDbUrl}/agendamento/${agendamentoEscolhido.id}`)
                     .then(() => {
-                        client.sendText(chatId, `*Seu agendamento do dia ${agendamentoEscolhido.Data} para às ${agendamentoEscolhido.Horario} foi desmarcado. Qualquer coisa estou à disposição.*`);
+                        client.sendText(chatId, `Seu agendamento do dia *${agendamentoEscolhido.data}* para às *${agendamentoEscolhido.horario}* foi desmarcado. Qualquer coisa estou à disposição.`);
                         // volta para o menu principal
                         currentState.step = 0;
                     })
@@ -190,35 +201,99 @@ const start = (client) => {
         // AGENDAMENTO - Finalizando e salvando agendamento na planilha (step === 4)
         } else if (currentState.step === 4) {
             if (currentState.servicoSelecionado === "marcar horario") {
-                currentState.nomeCliente = message.body;
-                currentState.id = uuidv4();
+                if (await isStringNumeroOuLista(message.body)){
+                    let response = await axios.get(`${sheetsDbUrl}servicos`);
+                    var servicos = response.data.servicos;
 
-                client.sendText(chatId, `*Obrigado, ${currentState.nomeCliente}! Seu agendamento foi concluído ✅*`);
+                    let servicos_selecionados = message.body.split(',').map(item => item.trim());
+                    var servicos_completos = '';
 
-                // Armazenamento do agendamento
-                const agendamento = {
-                    Id: currentState.id,
-                    Data: currentState.data,
-                    Horario: currentState.horarioEscolhido,
-                    Nome: currentState.nomeCliente,
-                    Servico: "",
-                    Telefone: currentState.telefone
-                };
+                    let contador = 0;
+                    servicos_selecionados.forEach(index_servico => {
+                        if (contador > 0){
+                            servicos_completos += ',';
+                            servicos_completos += servicos[index_servico - 1].servico;
+                        } else if (contador == 0) {
+                            servicos_completos += servicos[index_servico - 1].servico;
+                        }
+                        contador++;
+                    });
 
-                axios.post(sheetsDbUrl, agendamento)
+                    currentState.servico = servicos_completos;
+                    client.sendText(chatId, `*Agora digite o seu nome e sobrenome, por favor.*`);
+                    currentState.step = 5;
+
+                } else {
+                    currentState.step = 4; // Ajuste aqui para não voltar ao passo 3
+                    client.sendText(chatId, '*O serviço selecionado não é válido, digite apenas as opções mostradas no menu*');
+
+                    let response = await axios.get(`${sheetsDbUrl}servicos`);
+                    let contador = 0;
+                    var servicos = response.data.servicos;
+
+                    let mensagem_servicos = "";
+                    servicos.forEach(servico => {
+                        mensagem_servicos += ` *${contador + 1}. Serviço: ${servico.servico} | Preço: R$${servico.valor}*\n`
+                        contador++;
+                    });
+
+                    let mensagem_final = mensagem_servicos + '\n *Selecione o serviço que deseja. Você pode selecionar mais de um da seguinte forma: 1,2,3*'
+                    client.sendText(chatId, mensagem_final);
+                }
+            }
+        } else if (currentState.step === 5) {
+            client.sendText(chatId, `*Obrigado, ${message.body}. Seu horário foi marcado.*`);
+
+            currentState.id = uuidv4();
+            // Armazenamento do agendamento
+            const agendamento = {
+                id: uuidv4(),
+                data: currentState.data,
+                horario: currentState.horarioEscolhido,
+                nome: message.body,
+                servico: currentState.servico,
+                telefone: currentState.telefone 
+            };
+
+            axios.post(`${sheetsDbUrl}agendamento`, { agendamento })
                 .then(response => {
                     console.log('Dados salvos com sucesso:', response.data);
                 })
                 .catch(error => {
                     console.error('Erro ao enviar dados:', error);
                 });
-                console.log(agendamento);
+            console.log(agendamento);
 
-                // Resetar o estado para permitir novas interações
-                currentState.step = 0;
-                currentState.servicoSelecionado = "";
-            }
+            // Resetar o estado para permitir novas interações
+            currentState.step = 0;
+            currentState.servicoSelecionado = "";
         }
     });
-    
 };
+
+async function isStringNumeroOuLista(str) {
+    // Verifica se a string não é nula ou vazia
+    if (typeof str !== 'string' || str.trim() === '') {
+        return false;
+    }
+
+    // Expressão regular para verificar se cada item é um número inteiro
+    const regex = /^\d+$/;
+
+    // Divide a string em uma lista usando a vírgula como delimitador
+    const items = str.split(',');
+
+    //verifica se esse item existe como um serviço
+    let response = await axios.get(`${sheetsDbUrl}servicos`);
+    let servicos = response.data.servicos;
+
+    // Verifica se todos os itens na lista são números inteiros
+    for (let item of items) {
+        let index = parseInt(item.trim(), 10) - 1;
+        if (!regex.test(item.trim()) || index < 0 || index >= servicos.length) {
+            return false;
+        } 
+    }
+
+    return true;
+}
--- dataService.js
+++ dataService.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const apiUrl = 'https://sheetdb.io/api/v1/sapz5eicgzz66';
+const sheetsDbUrl = 'https://api.sheety.co/be2a6b443056f481f94cc19d22b20b70/barbearia/';
 
 
 //verifica se a data é válida para ser marcada para atendimento
@@ -38,19 +38,22 @@ function diaDaSemana(data){
     const ano = parseInt(dataSeparada[2])
     
     const dataFornecida = new Date(ano, mes, dia);
-    const diasDaSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+    const diasDaSemana = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado'];
     
     return diasDaSemana[dataFornecida.getDay()];
 }
 
  async function disponivel(data) {
-    const url = `${apiUrl}/search?sheet=indisponivel&data=${data.toString()}`;
+    data = data.replace("(", "");
+    data = data.replace(")", "");
+
+    const url = `${sheetsDbUrl}indisponivel?filter[data]=${data.toString()}`;
 
     try {
         const response = await axios.get(url);
 
-        if (response.data.length > 0) {
-            if (response.data[0].diaTodo == "x" || response.data[0].horarios.trim() == ""){
+        if (response.data.indisponivel.length > 0) {
+            if (response.data.indisponivel[0].diaTodo == "x" || response.data.indisponivel[0].horarios.trim() == ""){
                 return false;
 
             } else {
@@ -66,9 +69,90 @@ function diaDaSemana(data){
     }    
 }
 
+// function atualizarDisponibilidade(horarios, data){
+//     const filtrarDataEHorario = (agendamentos) => {
+//         return agendamentos.map(agendamento => ({
+//           Data: agendamento.data,
+//           Horario: agendamento.horario
+//         }));
+//       };
+
+//     axios.get(`${sheetsDbUrl}/agendamento`)
+//         .then((response) => {
+
+//         })
+//         .catch((error) => {
+//             error.log('erro ao consultar data');
+//         })
+
+
+
+
+async function obterHorariosPeloDia(dia) {
+    try {
+        const response = await axios.get(`${sheetsDbUrl}horarios`);
+        let lista;
+        switch(dia) {
+            case 'segunda':
+                lista = response.data.horarios.map(horario => horario.segunda).filter(horario => horario != undefined);
+                break;
+            case 'terça':
+                lista = response.data.horarios.map(horario => horario.terça).filter(horario => horario != undefined);
+                break;
+            case 'quarta':
+                lista = response.data.horarios.map(horario => horario.quarta).filter(horario => horario != undefined);
+                break;
+            case 'quinta':
+                lista = response.data.horarios.map(horario => horario.quinta).filter(horario => horario != undefined);
+                break;
+            case 'sexta':
+                lista = response.data.horarios.map(horario => horario.sexta).filter(horario => horario != undefined);
+                break;
+            case 'sábado':
+                lista = response.data.horarios.map(horario => horario.sábado).filter(horario => horario != undefined);
+                break;
+            default:
+                console.error('Dia da semana inválido');
+                return [];
+        }
+        return lista;
+    } catch (error) {
+        console.error("Erro ao obter horários:", error);
+        return [];
+    }
+}
+
+
+async function obterHorarioDisponivel(data){
+    //filtragem dos horários marcados
+    var diaSemana = diaDaSemana(data)
+    var horarios_disponiveis = await obterHorariosPeloDia(diaSemana);
+    var response = await axios.get(`${sheetsDbUrl}agendamento?filter[data]=${data}`);
+
+    var horarios_marcados = response.data.agendamento;
+    horarios_marcados = horarios_marcados.map(horario => horario.horario);
+
+    var horarios_disponiveis = horarios_disponiveis.filter(horario => !horarios_marcados.includes(horario));
+
+    //filtragem dos horários que já passaram
+    var hoje = new Date();
+    var dia = String(hoje.getDate()).padStart(2, '0');
+    var mes = String(hoje.getMonth() + 1).padStart(2, '0'); // Mês começa do 0
+    var ano = hoje.getFullYear();
+    var dataAtual = `${dia}/${mes}/${ano}`; // Formato 'dd/mm/aaaa'
+    var horaAtual = hoje.toTimeString().split(' ')[0].substring(0,5);
+
+    if (data == dataAtual){
+        horarios_disponiveis = horarios_disponiveis.filter(horario => horario > horaAtual);
+    }
+
+    return horarios_disponiveis;
+}
+
 
 module.exports = {
     validarData,
     diaDaSemana,
-    disponivel
+    disponivel,
+    obterHorarioDisponivel
 }
